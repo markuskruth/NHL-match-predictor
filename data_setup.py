a06f4c169db44e41734bef58370830338d1ff909
Markus Kruth <markus.kruth@aalto.fi>
2023-12-13 17:22:45 +0200

    Add statistical analysis methods for getting bookmaker odds

--- data_setup.py
+++ data_setup.py
@@ -1,11 +1,14 @@
 import pandas as pd
+import math
+from scipy.stats import norm
 
 def process_data(filename):
 	df = pd.read_csv(filename)
 
-	columns_to_keep = ["gameId","season","playerTeam","opposingTeam","home_or_away","goalsFor","goalsAgainst"]
+	columns_to_keep = ["gameId","season","playerTeam","opposingTeam","home_or_away","goalsFor","goalsAgainst","shotsOnGoalFor","shotsOnGoalAgainst","iceTime"]
 	df = df[columns_to_keep]
 	df = df[df["season"] > 2018]
+	df = df[df["iceTime"] == 3600]
 
 	def calculate_normalized_team_winrates(df):
 		# collect the names of every team
@@ -58,19 +61,32 @@ def process_data(filename):
 				home = game["playerTeam"]
 				away = game["opposingTeam"]
 				goalDifference = game["goalsFor"] - game["goalsAgainst"]
+				shotsOnGoalDif = (game["shotsOnGoalFor"] - game["shotsOnGoalAgainst"])/20
 			else:
 				home = game["opposingTeam"]
 				away = game["playerTeam"]
 				goalDifference = game["goalsAgainst"] - game["goalsFor"]
+				shotsOnGoalDif = (game["shotsOnGoalAgainst"] - game["shotsOnGoalFor"])/20
 
 			
 			winrate_difference = team_winrates[home] - team_winrates[away]
-			predictions.append(round(goalDifference + winrate_difference, 2))
+
+
+			predictions.append(round((goalDifference + winrate_difference + shotsOnGoalDif)/2, 2))
 
 		df["prediction"] = predictions
 
 	calculate_predictions(df)
 	columns_to_keep = ["playerTeam","opposingTeam","home_or_away","prediction"]
+	df = df[columns_to_keep]
+
+	STD = calculate_std(df)
+	confidences = calculate_confidences(df, STD)
+	odds_home,odds_away = calculate_bookmarker_odds(confidences)
+	df["oddsHome"] = odds_home
+	df["oddsAway"] = odds_away
+
+	columns_to_keep = ["playerTeam","opposingTeam","home_or_away","oddsHome", "oddsAway"]
 	df = df[columns_to_keep]
 
 	return df
@@ -80,3 +96,51 @@ def print_teams():
 	all_teams = df["playerTeam"].unique()
 	for team in all_teams:
 		print(team)
+
+def print_labels():
+	df = pd.read_csv("all_teams.csv")
+	data = df.iloc[-124]
+	for i in range(len(df.columns)):
+		print(df.columns[i],":",data[i])
+
+def calculate_std(df):
+	# calculate EV
+	predictions = df["prediction"].values.tolist()
+	EV = sum(predictions) / len(predictions)
+
+	# calculate variance
+	squared_differences = []
+	for p in predictions:
+		squared_differences.append((p - EV)**2)
+	VAR = sum(squared_differences) / len(squared_differences)
+
+	# calculate standard deviation
+	STD = math.sqrt(VAR)
+
+	return STD
+
+def calculate_confidences(df, STD):
+	predictions = df["prediction"].values.tolist()
+	confidences = []
+	for p in predictions:
+		# z = (EV-p) / (STD/sqrt(n))
+		z = p / STD
+		confidence = norm.cdf(z)
+		difference_to_50 = abs(0.5 - confidence)
+		if confidence < 0.5:
+			fixed_confidence = confidence + difference_to_50/2
+		else:
+			fixed_confidence = confidence - difference_to_50/2
+
+		confidences.append(fixed_confidence)
+
+	return confidences
+
+def calculate_bookmarker_odds(confidences):
+	oddsHome = []
+	oddsAway = []
+	for p in confidences:
+		oddsHome.append((1/p))
+		oddsAway.append(1/(1-p))
+
+	return oddsHome, oddsAway
