9bc8cfa0a048722dac9bb870a3beac5004131ec0
Markus Kruth <markus.kruth@aalto.fi>
2023-12-14 16:36:08 +0200

    Update GUI

--- GUI.py
+++ GUI.py
@@ -24,22 +24,14 @@ def get_teams():
 	return home_team, away_team
 
 def update_odds(home_odds, away_odds):
-	odds_home_variable.set(f"Home odds: {home_odds}")
-	odds_away_variable.set(f"Away odds: {away_odds}")
+	odds_home_variable.set(home_odds)
+	odds_away_variable.set(away_odds)
 
 if __name__ == "__main__":
 
 	win = tk.Tk()
-	win.title("NHL match predictor") 
-	win.geometry("600x300")
-
-	title = ttk.Label(win,
-		    text="NHL match odds predictor",
-		    foreground="white",
-		    background="black",
-		    font = ("Times New Roman", 15))
-
-	#label.pack()
+	win.title("NHL match odds predictor") 
+	win.geometry("600x200")
 
 	home_row = 5
 	home_column = 0
@@ -73,25 +65,38 @@ if __name__ == "__main__":
 							" NJD"," TBL"," CBJ"," CHI"," NSH"," CGY",
 							" BOS"," DET"," VAN"," CAR"," S.J"," L.A")
 
-	button = ttk.Button(win, text="Predict", command=predict_collect)
+	button = ttk.Button(win, text="Predict", command=predict_collect, width = 15)
 
 	odds_home_variable = tk.StringVar()
 	odds_away_variable = tk.StringVar()
 
-	odds_home = ttk.Label(win, textvariable=odds_home_variable, font = ("Times New Roman", 15))
-	odds_away = ttk.Label(win, textvariable=odds_away_variable, font = ("Times New Roman", 15))
+	odds_home = ttk.Label(win, textvariable=odds_home_variable,
+		foreground="white",
+	    background="Green",
+		font = ("Times New Roman", 25))
 
-	odds_home_variable.set(f"Home odds: {prediction[0]}")
-	odds_away_variable.set(f"Away odds: {prediction[1]}")
+	odds_away = ttk.Label(win, textvariable=odds_away_variable,
+		foreground="white",
+	    background="Green",
+		font = ("Times New Roman", 25))
 
-	title.grid(column = 1, row = 0)
+	odds_home_label = ttk.Label(win, text="Home odds:",
+								font = ("Times New Roman", 15))
+	odds_home_variable.set(prediction[0])
+
+	odds_away_label = ttk.Label(win, text="Away odds:",
+								font = ("Times New Roman", 15))
+	odds_away_variable.set(prediction[1])
+
 	home_label.grid(column = home_column, row = home_row, padx = 10, pady = 25)
 	home_chosen.grid(column = home_column+1, row = home_row)
 	away_label.grid(column = away_column, row = away_row, padx = 10, pady = 25)
 	away_chosen.grid(column = away_column+1, row = away_row)
 	#team_chosen.current(0)
-	button.grid(column = 0, row = 7)
-	odds_home.grid(column = 2, row = home_row, padx = 20)
-	odds_away.grid(column = 2, row = away_row, padx = 20)
+	button.grid(column = 0, row = 7, padx = 5)
+	odds_home_label.grid(column = 2, row = home_row, padx = 20)
+	odds_home.grid(column = 3, row = home_row)
+	odds_away_label.grid(column = 2, row = away_row, padx = 20)
+	odds_away.grid(column = 3, row = away_row)
 
 	win.mainloop()
